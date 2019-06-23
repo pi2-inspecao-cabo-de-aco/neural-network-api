d10e42c1205ed7b9d6b180cc290661093473ef50
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2019-06-23 20:11:20 -0300

    Adding learner and method to read image bytes (need fixes)

--- app/main.py
+++ app/main.py
@@ -1,14 +1,36 @@
 from fastapi import FastAPI
+from fastai import *
+from fastai.vision import *
+from starlette.responses import JSONResponse
+import io
+from os.path import dirname, abspath, join
+from PIL import Image
+
+model_path = Path('./models/resnet50')
+
+dirname = dirname(dirname(abspath(__file__)))
+images_path = join(dirname, '/public/')
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-  return {'Hello': 'World'}
+learn = load_learner(model_path, 'model.pkl')
 
-@app.get('/items/{item_id}')
-def read_item(item_id: int, q: str = None):
-  return {
-    'item_id': item_id,
-    'q': q
-  }
+def read_byte_img(image_path):
+  img = Image.open(image_path, mode='r')
+  imgByteArr = io.BytesIO()
+  img.save(imgByteArr, format='PNG')
+
+  return imgByteArr
+
+@app.get("/")
+def root():
+  return {"Inspeção de Cabo de Aço - Neural Network API"}
+
+@app.post("/analyze")
+def analyze(image_name: str):
+  image_name = image_name + '.png'
+  img = images_path + image_name
+  byte_img = read_byte_img(img)
+  prediction = learn.predict(byte_img)[0]
+
+  return JSONResponse({'result': str(prediction)})
